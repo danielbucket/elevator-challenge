37a9ccf0204e87cbc712229248243e8c2bd5905a
Daniel Bucket <danieljbucket@gmail.com>
2017-08-28 00:16:44 -0600

    initial commit

--- elevator.js
+++ elevator.js
@@ -1,5 +1,37 @@
-export default class Elevator {
+export class Elevator {
   constructor() {
-    
+    this.defaultFloor       = 0,
+    this.currentFloor       = null,
+    this.destination        = [],
+    this.passengers         = [],
+    this.dropOffFloor       = 0,
+    this.floorsTraveresed   = 0,
+    this.stops              = 0
+  }
+
+  reset() {
+
+  }
+
+  goToFloor(person) {
+    this.passengers.push(person)
+    this.destination.push(person.dropOffFloor)
+    this.currentFloor = person.dropOffFloor
+    this.floorsTraveresed = this.floorsTraveresed + (this.defaultFloor + person.currentFloor) + (person.currentFloor - person.dropOffFloor)
+
+
+    // this.stops = this.stops + 
+
+
+  }
+
+}
+
+
+export class Person {
+  constructor(name, currentFloor, dropOffFloor) {
+    this.name         = name,
+    this.currentFloor = currentFloor,
+    this.dropOffFloor = dropOffFloor
   }
 }
--- tests/elevator_test.js
+++ tests/elevator_test.js
@@ -3,26 +3,73 @@ require('babel-core/register')({
 });
 
 const assert = require('chai').assert;
-const Elevator = require('../elevator').default;
+const { Elevator, Person }  = require('../elevator')
 
 describe('Elevator', function() {
-  let elevator = new Elevator();
+  let elevator  = new Elevator()
 
   beforeEach(function() {
-    elevator.reset();
+    elevator.reset()
   });
 
-  it('should bring a rider to a floor above their current floor', () => {
-    let mockUser = { name: "Brittany", currentFloor: 2, dropOffFloor: 5 };
-    elevator.goToFloor(mockUser);
+  it('01: should be a function', () => {
+    assert.isFunction(Elevator)
+  })
 
-    assert.equal(elevator.currentFloor, 5);
+  it('02: should instantiate a new elevator', () => {
+    assert.isObject(elevator)
+  })
+
+  it('03: should have a default of 0', () => {
+    assert.equal(elevator.defaultFloor, 0)
+  })
+
+  it('04: shoould be an array', () => {
+    assert.deepEqual(elevator.destination, [])
+  })
+
+  it('05: should be a function', () => {
+    assert.isFunction(elevator.goToFloor)
+  })
+
+  it('06: should bring a rider to a floor above their current floor', () => {
+    let mockUser = new Person('Brittany', 2, 5)
+
+    elevator.goToFloor(mockUser)
+
+    assert.equal(elevator.currentFloor, 5)
   });
 
-  it('should bring a rider to a floor below their current floor', () => {
-    let mockUser = { name: "Brittany", currentFloor: 8, dropOffFloor: 3 };
-    elevator.goToFloor(mockUser);
+  it.only('07: should bring a rider to a floor below their current floor', () => {
+    let mockUser = new Person('Chad', 8, 3)
 
-    assert.equal(elevator.currentFloor, 3);
-  });
-});
+    elevator.goToFloor(mockUser)
+
+    assert.equal(elevator.currentFloor, 3)
+    assert.equal(elevator.floorsTraveresed, 13)
+    // assert.equal(elevator.stops, 2)
+  })
+
+
+  it('10: ', () => {
+    let mockUser = new Person('Brad', 8, 2)
+
+
+
+
+  })
+})
+
+describe('Person', () => {
+
+  it('00: should be a function', () => {
+
+    assert.isFunction(Person)
+  })
+
+  it('01: should be instantiate Person', () => {
+    const person = new Person()
+
+    assert.isObject(person)
+  })
+})
